d8470fa9e6b9eec3f85b514785801773b668ae63
suryansh-codes15 <suryanshsrivastava215@gmail.com>
2026-04-16 18:41:05 +0530

    Fix final bundlesize and CVE preventing Vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pdf2json', 'sharp'],
+  experimental: {
+    outputFileTracingExcludes: {
+      "/*": ["./node_modules/canvas/**/*", "./node_modules/jsdom/**/*", "./node_modules/pdfjs-dist/**/*"],
+      "/api/**/*": ["./node_modules/canvas/**/*", "./node_modules/jsdom/**/*", "./node_modules/pdfjs-dist/**/*"]
+    }
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [...(config.externals || []), 'canvas', 'jsdom'];
